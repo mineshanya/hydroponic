7aabc63a3b4a8cea69860f7f46199bc626fa0bd4
mineshanya <mishan99@mail.ru>
2021-12-04 00:26:57 +0300

    Update Hydroponic.ino
    make menus scrollable

--- Hydroponic/Hydroponic.ino
+++ Hydroponic/Hydroponic.ino
@@ -52,14 +52,14 @@
 /*#################################  РАСПИСАНИЕ ПО-УМОЛЧАНИЮ  ############################################################################################################*/
 #define oled_timeout 10         // время в секундах до автовыключения дисплея, 1-240 сек, 0 горит всегда
 #define WTR_ON 0                // время вкл полива (минуты), 0-59
-#define WTR_OFF 10              // время выкл полива (минуты), 0-59
+#define WTR_OFF 5               // время выкл полива (минуты), 0-59
 #define LGHT_ON 8               // время вкл света (часы), 0-23
 #define LGHT_OFF 21             // время выкл света (часы), 0-23
-#define BRTNSS_ON 45            // уровень освещенности вкл света, обычно 52, 0-100
-#define BRTNSS_OFF 55           // уровень освещенности выкл света, обычно 52, 0-100
+#define BRTNSS_ON 40            // уровень освещенности вкл света, обычно 52, 0-100
+#define BRTNSS_OFF 60           // уровень освещенности выкл света, обычно 52, 0-100
 
 /*########################################################################################################################################################################*/
-#define projectversion F("version\n\r4.0.0")
+#define projectversion F("version\n\r3.2.1")
 #define SerialSpeed 57600
 #define separator F("----------------------------------")
 #include <EncButton.h>
@@ -125,7 +125,7 @@ uint32_t myTimer2 = 5*1000;
 const byte justzero = 0;
 const byte secondmax = 59;
 const byte minutemax = 59;
-const byte hourmax = 59;
+const byte hourmax = 23;
 const byte percentmax = 100;
 const byte pinmax = 19;
 const byte analogpinmin = 14;
@@ -144,6 +144,7 @@ void blink (byte i){
 // отображение message на дисплее текстом размера 1 или 2 и продолжительностью ms/1000 секунд
 void showMessage(byte text_size,String message,int ms=3500){
   Serial.println(message);
+  Serial.println(separator);
   if(useOLED){
     OLED.clear();
     OLED.setCursor(0, 0);
@@ -222,7 +223,7 @@ void sendToDisp (hydroponic& pot, DateTime& now) {
 }
 
 // прокручиваем значение val по кругу между min и max
-void spinVal (byte& val, byte min, byte max, int8_t direction) {
+void spinVal (byte& val, const byte min, const byte max, int8_t direction) {
   if (direction>0)
     if (val<max) val++; else val = min;
   else if (val>min) val--; else val = max;
@@ -233,137 +234,176 @@ void spinVal (paramptr& elem, int8_t direction) {
   else if (*elem.val > *elem.min) (*elem.val)--; else *elem.val = *elem.max;
 }
 
+byte calcShift(const byte& listsize, const byte& nSel, const byte& maxlines){
+  if (listsize>maxlines && nSel>maxlines) {
+      if (nSel<=listsize)
+        return nSel-maxlines;
+      //else if (nSel<)
+    }
+  return 0;
+}
+
 // компоновка меню для отображения на дисплее
-void menuCompose(const String menulist[][2], const byte &menusize, const byte &n, const String &header = (const String)(""), byte NextColAfterItem = 3, byte NextColX = 98){
+void menuCompose(const byte &nSel, String paramlist[][2], const byte &paramlistsize, const String actionlist[], const byte actionlistsize = 0, byte NextColX = 94){
   OLED.clear();
   OLED.setScale(1);
   OLED.setCursor(0, 0);
-  if(header!="") {OLED.println(header); NextColAfterItem++;}
-  for (byte i=0; i<menusize; i++){
-    OLED.invertText(i+1==n);
-    if (i>NextColAfterItem) {OLED.setCursor(NextColX, i-NextColAfterItem-1);}
-    OLED.print(menulist[i][0]);
-    OLED.invertText(false);
-    OLED.print(menulist[i][1]);
+  byte printheader=paramlist[0][0]!="";
+  byte maxlines = 4 - printheader;
+  byte shift = 0;
+  byte printmode = byte(paramlist[0][1][0])-48;                         // 48 Это код символа '0', получаем цифру
+  if(printheader) {OLED.print(paramlist[0][0]);}  // Заголовок окна
+  shift=calcShift(paramlistsize-1,nSel,maxlines);
+  for (byte i=1; i<paramlistsize && i<=maxlines; i++){
+    OLED.setCursor(0, i-printheader);
+    OLED.invertText(i+shift==nSel);
+    OLED.print(paramlist[i+shift][0]);
+    if (printmode) {
+      OLED.invertText(false);
+      OLED.print(paramlist[i+shift][1]);
+    }
   }
+  if (actionlistsize) {
+    shift=calcShift(actionlistsize,nSel-paramlistsize+1,maxlines);
+    OLED.fastLineV(NextColX, printheader*8, 31, OLED_FILL);
+    NextColX+=4;
+    for (byte i=0; i<actionlistsize && i<=maxlines; i++){
+      OLED.setCursor(NextColX, i+printheader);
+      OLED.invertText(i+shift==nSel-paramlistsize);
+      OLED.println(actionlist[i+shift]);
+    }
+  }
+  OLED.invertText(false);
 }
+
 void menuShow(){
   OLED.update();
 }
 
 
 // типовое подменю дисплея для изменения текущего времени и параметров расписания
-bool menuValChange(const String& header, paramptr paramArray[], const byte& size){
-  byte nSel = 0;
+bool menuValChange(const String& header, paramptr paramArray[], const byte& paramArraySize){
   Serial.println((String)F("Editing ") + header);
-    bool cancel = false;
-    menuValChange(header, paramArray, size, ++nSel);
-    byte valueBkp[size];
-    for (byte i=0;i<size;i++) valueBkp[i] = *paramArray[i].val;
-    do {                                                                                  // переходим в режим ввода с энкодера
-      enc.tick();                                                                         // опрашиваем энкодер
-      if (enc.turn()) {                                                                   // выделяем другой пункт меню при вращении
-        spinVal(nSel, 1, size+2, enc.getDir());
-        menuValChange(header, paramArray, size, nSel);
-      }
-      else if (enc.turnH() && nSel<=size) {                                               // редактируем выбранный пункт при нажатом вращении
-        spinVal(paramArray[nSel-1], enc.getDir());
-        menuValChange(header, paramArray, size, nSel);
-      }
-      else if (enc.click()) switch(nSel-size){                                            // если был нажат выбранный пункт
-        case 1:{                                                                          // пункт "Ok", сохранение установленных значений
-          return true;
-          break;
-          }
-        case 2:{                                                                          // пункт "Cancel", выход из подменю без сохранения
-          for (byte i=0;i<size;i++) *paramArray[i].val=valueBkp[i];
-          return false;
-          break;
-          }
-      }
-    } while (!cancel);
-    return false;
+  byte nSel = 0;
+  menuValChange(header, paramArray, paramArraySize, ++nSel);
+  bool cancel = false;
+  byte valueBkp[paramArraySize];
+  for (byte i=0;i<paramArraySize;i++) valueBkp[i] = *paramArray[i].val;
+  do {                                                                                  // переходим в режим ввода с энкодера
+    enc.tick();                                                                         // опрашиваем энкодер
+    if (enc.turn()) {                                                                   // выделяем другой пункт меню при вращении
+      spinVal(nSel, 1, paramArraySize+2, enc.getDir());
+      menuValChange(header, paramArray, paramArraySize, nSel);
+    }
+    else if (enc.turnH() && nSel<=paramArraySize) {                                               // редактируем выбранный пункт при нажатом вращении
+      spinVal(paramArray[nSel-1], enc.getDir());
+      menuValChange(header, paramArray, paramArraySize, nSel);
+    }
+    else if (enc.click()) switch(nSel-paramArraySize){                                            // если был нажат выбранный пункт
+      case 1:{                                                                          // пункт "Ok", сохранение установленных значений
+        return true;
+        break;
+        }
+      case 2:{                                                                          // пункт "Cancel", выход из подменю без сохранения
+        for (byte i=0;i<paramArraySize;i++) *paramArray[i].val=valueBkp[i];
+        return false;
+        break;
+        }
+    }
+  } while (!cancel);
+  return false;
 }
-void menuValChange(const String& header, paramptr paramArray[], const byte& size, byte& nSel){
-  String menulist[size+2][2];
-  for (byte i=0;i<size;i++){
-          if (*paramArray[i].val<10) menulist[i][0]='0'+String(*paramArray[i].val);
-          else menulist[i][0]=String(*paramArray[i].val);
-          menulist[i][1]=paramArray[i].name;
+void menuValChange(const String& header, paramptr paramArray[], const byte& paramArraySize, byte& nSel){
+  const String menulist[paramArraySize+1][2];
+  for (byte i=0;i<paramArraySize;i++){
+          if (*paramArray[i].val<10) menulist[i+1][0]='0'+String(*paramArray[i].val);
+          else menulist[i+1][0]=String(*paramArray[i].val);
+          menulist[i+1][1]=paramArray[i].name;
   }
-  menulist[size][0] = F("\n\rOk");
-  menulist[size][1] = F(" ");
-  menulist[size+1][0] = F("Cancel");
-  menuCompose(menulist,size+2, nSel, header);                                           // компоновка меню
-  if (nSel<=size) {
-    OLED.setCursor(86, 1);
-    OLED.print((String)F("(")+String(*paramArray[nSel-1].min)+F("-")+String(*paramArray[nSel-1].max)+F(")"));
+  menulist[0][0] = header;
+  menulist[0][1] = F("1");
+  const String actionlist[]={{F("OK")},{F("Cancel")}};
+  menuCompose(nSel,menulist,paramArraySize+1,actionlist,2,86);
+  if (nSel<=paramArraySize) {
+    OLED.fastLineH(3*8-2, 86, 128, OLED_FILL);
+    OLED.setCursor(90, 3);
+    OLED.print(String(*paramArray[nSel-1].min)+F("-")+String(*paramArray[nSel-1].max));
   }
   menuShow();
 }
 
 // отрисовка главного меню, nSel-й элемент будет выделен, нумерация с 0
 void menuMain (hydroponic& pot, DateTime& now, byte nSel = 0){
-  const String menulist[][2]={
-                        {{F("Watering\n\r")},   {F("")}},
-                        {{F("Lighting\n\r")},   {F("")}},
-                        {{F("Brightness\n\r")}, {F("")}},
-                        {{F("Time\n\r")},       {F("")}},
-                        {{F("Save")},           {F("")}},
-                        {{F("Load")},           {F("")}},
-                        {{F("Reset")},          {F("")}},
-                        {{F("Close")},          {F("")}}
+  const String paramlist[][2]={
+                        {{F("")},           {F("0")}},
+                        {{F("Watering")},   {F(" (mins, hrly)")}},
+                        {{F("Lighting")},   {F(" (hrs, daily)")}},
+                        {{F("Brightness")}, {F(" (%,hyst)")}},
+                        {{F("Time")},       {F("")}},
+                        {{F("Another")},    {F("")}},
+                        {{F("Brilliant")},  {F("")}},
+                        {{F("Setup")},      {F("")}},
                         };
+  const byte paramlistsize = 8;
+  const String actionlist[]={
+                        {F("Save")},
+                        {F("Load")},
+                        {F("Reset")},          
+                        {F("Close")},         
+                        };
+  const byte actionlistsize=sizeof(actionlist)/sizeof(actionlist[0]);
   if (nSel){
-    menuCompose(menulist,8, nSel);                      // 8 кол-во эл меню
-    OLED.fastLineV(94, 0, 31, OLED_FILL);               // компоновка линии по координатам (94,0) -> (94,31)
+    menuCompose(nSel, paramlist, paramlistsize, actionlist, actionlistsize);
     menuShow();                                         // вывод меню и линии на экран
   }
   else {
     bool cancel = false;
-    Serial.println(F("Menu Entered"));
+    Serial.println(F("Main Menu Entered"));
     showMessage(2,F(" Settings"),2000);
     menuMain(pot, now, ++nSel);                         // отрисовка главного меню с выделенным первым элементом
     do{                                                 // запуск бесконечного цикла опроса энкодера, пока не будет нажат пункт "Cancel"
       enc.tick();                                       // опрос состояния энкодера
       if (enc.turn()) {
-        spinVal(nSel, 1, 8, enc.getDir());
+        spinVal(nSel, 1, paramlistsize-1+actionlistsize, enc.getDir());
         menuMain(pot, now, nSel);
       }
       if (enc.click()){                                 // если был нажат выбранный пункт
-        switch(nSel){
+        if (nSel<paramlistsize) switch(nSel){
           case 1: {                                                                                       // пункт настройки полива
-            paramptr paramArrayptr[3] = { {F(" pin\n\r"),   pot.w_pin,      justzero,       pinmax},
-                                          {F("(m) on - "),  pot.w_on,       justzero,       pot.w_off},
+            paramptr paramArrayptr[3] = { {F(" pin"),   pot.w_pin,      justzero,       pinmax},
+                                          {F("(m) on"),  pot.w_on,       justzero,       pot.w_off},
                                           {F("(m) off"),    pot.w_off,      pot.w_on,       minutemax}
                                           };
-            menuValChange(F("Water (mins, hrly)"),paramArrayptr,sizeof(paramArrayptr)/sizeof(paramArrayptr[0]));
+            menuValChange(String(paramlist[1][0])+String(paramlist[1][1]),paramArrayptr,sizeof(paramArrayptr)/sizeof(paramArrayptr[0]));
             break;}
           case 2: {                                                                                       // пункт настройки освещения
             paramptr paramArrayptr[3] = { {F(" pin\n\r"),   pot.l_pin,      justzero,       pinmax},
-                                          {F("(h) on - "),  pot.l_on,       justzero,       pot.l_off},
+                                          {F("(h) on\n\r"),  pot.l_on,       justzero,       pot.l_off},
                                           {F("(h) off"),    pot.l_off,      pot.l_on,       hourmax}
                                           };
-            menuValChange(F("Light (hrs, daily)"),paramArrayptr,sizeof(paramArrayptr)/sizeof(paramArrayptr[0]));
+            menuValChange(F("Lighting (hrs, daily)"),paramArrayptr,sizeof(paramArrayptr)/sizeof(paramArrayptr[0]));
             break;}
           case 3: {                                                                                       // пункт настройки порога вкл/выкл освещения
             paramptr paramArrayptr[3] = { {F(" pin\n\r"),hydromodule1.br_sens_pin,analogpinmin,analogpinmax},
-                                          {F("(%) on - "),  pot.br_lvl_on,  justzero,       pot.br_lvl_off},
+                                          {F("(%) on\n\r"),  pot.br_lvl_on,  justzero,       pot.br_lvl_off},
                                           {F("(%) off"),    pot.br_lvl_off, pot.br_lvl_on,  percentmax}
                                           };
             menuValChange(F("Brightness (%,hyst)"),paramArrayptr,sizeof(paramArrayptr)/sizeof(paramArrayptr[0]));
-             break;}
+            break;}
           case 4: {                                                                                       // пункт настройки времени
-            paramptr paramArrayptr[3] = { {F("h:"),         now.hour,       justzero,       hourmax},
-                                          {F("m:"),         now.minute,     justzero,       minutemax},
-                                          {F("s"),          now.second,     justzero,       secondmax}
+            updTime(now);
+            paramptr paramArrayptr[3] = { {F("(h)\n\r"),         now.hour,       justzero,       hourmax},
+                                          {F("(m)\n\r"),         now.minute,     justzero,       minutemax},
+                                          {F("(s)\n\r"),          now.second,     justzero,       secondmax}
                                           };
             if (menuValChange(F("Time"),paramArrayptr,sizeof(paramArrayptr)/sizeof(paramArrayptr[0]))) RTC.setTime(now.second, now.minute, now.hour, 1, 12, 2021);
             break;}
-          case 5: {saveToEEPROM(hydromodule1); cancel=true; break;}   // пункт "Save"
-          case 6: {loadFromEEPROM(hydromodule1); cancel=true; break;}   // пункт "Load"
-          case 7: {restoreDefaults(hydromodule1); cancel=true; break;}                                             // пункт "Reset"
-          case 8: {cancel=true; break;}                                                                   // пункт "Cancel"
+        }
+        else switch(nSel-paramlistsize+1){
+          case 1: {saveToEEPROM(hydromodule1); cancel=true; break;}   // пункт "Save"
+          case 2: {loadFromEEPROM(hydromodule1); cancel=true; break;}   // пункт "Load"
+          case 3: {restoreDefaults(hydromodule1); cancel=true; break;}                                             // пункт "Reset"
+          case 4: {cancel=true; break;}                                                                   // пункт "Cancel"
         }
         menuMain(pot, now, nSel);
       }
